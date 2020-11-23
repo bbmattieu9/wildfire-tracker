b53362b4ba1fa596a0a625b3bc72ee07e49ad0c3
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-11-23 13:39:37 +0100

    [App] first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>WildFire Tracker v1.0</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
