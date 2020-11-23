9380c9aa7043322416dfe7ac07cbd50513dda0ed
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-11-23 13:41:04 +0100

    [App] Add files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>WildFire Tracker v1.0</h3>
+        <h3>WildFire Tracker App</h3>
       </header>
     </div>
   );
