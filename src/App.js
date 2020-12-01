6b50d29ad4a971f36f505029af79687643ca577d
Babatoonday Mattieu <32313099+bbmattieu9@users.noreply.github.com>
2020-12-01 13:48:47 +0100

    [Map] Add data for map from NASA api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import Map from './components/Map'
+import  { useState, useEffect } from 'react'
 
 function App() {
+
+  const [eventData, setEventData] = useState([]);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+
+    const fetchEvents = async () => {
+      setLoader(true);
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+      const { events } = await res.json();
+      setEventData(events);
+      setLoader(false);
+    }
+
+    fetchEvents();
+
+    console.log('[Events from NASA api]:', eventData);
+  },)
+
   return (
     <div>
-       <Map />
+       { !loader ? <Map eventData={eventData} /> : <h1>Loading..</h1>}
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,6 @@
-import googleMapReact from 'google-map-react'
 import GoogleMapReact from 'google-map-react'
+import LocationMarker from './LocationMarker'
+
 
 const Map = ({center, zoom}) => {
     return (
@@ -10,7 +11,7 @@ const Map = ({center, zoom}) => {
                 defaultCenter={ center }
                 defaultZoom={ zoom }
             >
-
+                <LocationMarker lat={center.lat} lng={center.lng} />
             </GoogleMapReact>
         </div>
     )
